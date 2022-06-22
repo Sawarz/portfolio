edd97cd99439413a4cd81c261c30fcb674dd0713
unknown <sawarz22@gmail.com>
2022-06-22 23:55:08 +0200

    Initial roadmap commit

--- src/components/roadmap/Roadmap.js
+++ src/components/roadmap/Roadmap.js
@@ -1,8 +1,126 @@
-import React from 'react'
+import { useEffect } from 'react'
 import styles from './styles.module.css'
+import "animate.css/animate.min.css";
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import Confetti from 'react-confetti';
+
 
 export default function Roadmap() {
+  function AnimatedText(props) {
+    const [ref, inView] = useInView();
+    const control = useAnimation();
+
+    const textVariant = {
+      out: {
+        opacity: 0,
+        scale: 0,
+        x: "40vw"
+      },
+      in: {
+        opacity: 1,
+        scale: [0.5, 0.5, 1],
+        x: 0
+      }
+    }
+
+
+    useEffect(() => {
+      if (inView) {
+        control.start("in");
+      }
+      else {
+        control.start("out");
+      }
+    }, [control, inView]);
+
+    return (<motion.div
+      className={styles.roadmapText}
+      ref={ref}
+      variants={textVariant}
+      initial="out"
+      animate={control}
+    >{props.children}</motion.div>)
+  }
+
+  function AnimatedContent(props) {
+    const [ref, inView] = useInView();
+    const control = useAnimation();
+
+    const textVariant = {
+      out: {
+        opacity: 0,
+        y: "30vh"
+      },
+      in: {
+        opacity: 1,
+        y: "0vh"
+      }
+    }
+
+
+    useEffect(() => {
+      if (inView) {
+        control.start("in");
+      }
+      else {
+        control.start("out");
+      }
+    }, [control, inView]);
+
+    return (<motion.div
+      className={props.className}
+      ref={ref}
+      variants={textVariant}
+      initial="out"
+      animate={control}
+      transition={{duration: 1}}
+    >{props.children}</motion.div>)
+  }
+
+  
+
   return (
-    <div>Roadmap</div>
+    <div>
+      <div className={styles.roadmapLine}>
+        <div className={styles.roadmapPoint}>
+          <div className={styles.roadmapContent}>
+            <AnimatedText>Tomasz Sawarzyński - Aspiring Frontend Developer</AnimatedText>
+            <AnimatedContent className={styles.animatedContent}>👨‍💻</AnimatedContent>
+          </div>
+        </div>
+        <div className={styles.roadmapPoint}>
+          <div className={styles.roadmapContent}>
+            <AnimatedText>When it all started...</AnimatedText>
+            <AnimatedContent className={styles.animatedContent}>👨‍🎨</AnimatedContent>
+            <AnimatedContent className={styles.animatedContentText}>2018 - Simple python games</AnimatedContent>
+          </div>
+        </div>
+        <div className={styles.roadmapPoint}>
+          <div className={styles.roadmapContent}>
+            <AnimatedText>Emerging into...</AnimatedText>
+            <AnimatedContent className={styles.animatedContent}>🕵️</AnimatedContent>
+            <AnimatedContent className={styles.animatedContentText}>2020 - First frontend interest</AnimatedContent>
+          </div>
+        </div>
+        <div className={styles.roadmapPoint}>
+          <div className={styles.roadmapContent}>
+            <AnimatedText>Resulting in...</AnimatedText>
+            <AnimatedContent className={styles.animatedContent}>🤹</AnimatedContent>
+            <AnimatedContent className={styles.animatedContentText}>June 2021 - Attempts to learn JS, React</AnimatedContent>
+          </div>
+        </div>
+        <div className={styles.roadmapPoint}>
+        <div className={styles.confettiSource}></div>
+        <AnimatedContent className={styles.confetti}><Confetti></Confetti></AnimatedContent>
+          <div className={styles.roadmapContent}>
+            <AnimatedText>Recent times...</AnimatedText>
+            <AnimatedContent className={styles.animatedContent}>🏅</AnimatedContent>
+            <AnimatedContent className={styles.animatedContentText}>2022 - Finishing projects and applying for a job!</AnimatedContent> 
+          </div>
+        </div>
+        <div className={styles.roadmapEnd}></div>
+      </div>
+    </div>
   )
 }
